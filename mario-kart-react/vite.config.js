bb0068ebf6294fca6156b552e5b87df2d5b93657
vcastald <valentino.castaldini@gmail.com>
2026-01-06 14:53:31 +0100

    only https

--- mario-kart-react/vite.config.js
+++ mario-kart-react/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     host: true,      // <--- Aggiungi questo: permette l'accesso da fuori il container
     strictPort: true,
     port: 5173,
+	// 2. Aggiungi la configurazione HTTPS qui
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),  // Legge la chiave privata
+      cert: fs.readFileSync('./certs/cert.pem'), // Legge il certificato pubblico
+    },
     watch: {
       usePolling: true // <--- Aggiungi questo se usi Windows/WSL per fixare il reload
     }
