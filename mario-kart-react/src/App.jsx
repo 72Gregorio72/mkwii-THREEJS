6cce70ad792278a4808a10fd1c8e545bd7be80e2
vcastald <valentino.castaldini@gmail.com>
2026-01-29 10:39:06 +0100

    no comment

--- mario-kart-react/src/App.jsx
+++ mario-kart-react/src/App.jsx
@@ -38,7 +38,6 @@ export default function App() {
         	        <VehicleSelection 
         	            setMenuState={setMenuState} 
         	            selectedCharacter={SelectedCharacter}
-        	            // Pass the setter so we save the vehicle
         	            setSelectedVehicle={setSelectedVehicle} 
         	        />
         	    )}
@@ -58,8 +57,8 @@ export default function App() {
 
 						// Passiamo i dati dinamici dalla pista selezionata
 						mapPath={SelectedTrack.file} 
-						checkpointPath={SelectedTrack.checkpoints} // <--- NUOVO
-						maxCheckpoints={SelectedTrack.maxCheckpoints || 1} // <--- NUOVO
+						checkpointPath={SelectedTrack.checkpoints}
+						maxCheckpoints={SelectedTrack.maxCheckpoints || 1}
 						start_pos={SelectedTrack.startPos}
 						selectedTrack={SelectedTrack}
 						onBack={() => setMenuState(0)}
