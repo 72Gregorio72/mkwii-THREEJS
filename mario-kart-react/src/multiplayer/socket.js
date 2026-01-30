569f72f1b539069bf61599ff21a4b4dc72ed3db9
vcastald <valentino.castaldini@gmail.com>
2026-01-30 16:16:02 +0100

    correzione nginx.conf e socket.js

--- mario-kart-react/src/multiplayer/socket.js
+++ mario-kart-react/src/multiplayer/socket.js
@@ -7,9 +7,11 @@ const SERVER_URL = `${window.location.protocol}//${window.location.host}`;
 console.log('Connecting via Nginx Proxy to:', SERVER_URL);
 
 export const socket = io(SERVER_URL, {
-    transports: ['websocket', 'polling'], 
+    transports: ['websocket'],
+	upgrade: false, 
     withCredentials: true,
     rejectUnauthorized: false,
+	secure: true,
     // Opzionale: riconnessione più aggressiva
     reconnection: true,
     reconnectionAttempts: 10
