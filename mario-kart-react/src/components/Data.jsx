e7f333c84319dcb2aaa37b68a6fa48d61d1938ff
Francescotersi <francescotersillo@gmail.com>
2026-02-10 14:07:56 +0100

    Moo Moo Meadows quasi fatto. sono una mucca moo moo

--- mario-kart-react/src/components/Data.jsx
+++ mario-kart-react/src/components/Data.jsx
@@ -90,7 +90,19 @@ export const Tracks = {
         gridpos: './Tracks/YoshiFalls/YoshiFalls_startpos.glb',
         road: './Tracks/YoshiFalls/YoshiFalls_road.glb',
 		Waypoints: [ DelfinoSquare0, DelfinoSquareL, DelfinoSquareR ]
-    }
+    },
+    'Moo Moo Meadows': {
+        file: './Tracks/MooMooMeadows/MooMooMeadows.glb',
+        preview: './TrackPreviews/YoshiFalls.png',
+        startPos: [0, 0, 50],
+        checkpoints: './Tracks/MooMooMeadows/MooMooMeadows_checkpoints.glb',
+        maxCheckpoints: 3,
+        soundtrack: 'MOO_MOO_MEADOWS',
+        itemBoxes: './Tracks/MooMooMeadows/MooMooMeadows_itembox.glb',
+        gridpos: './Tracks/MooMooMeadows/MooMooMeadows_startpos.glb',
+        road: './Tracks/MooMooMeadows/MooMooMeadows_road.glb',
+		Waypoints: [ DelfinoSquare0, DelfinoSquareL, DelfinoSquareR ]
+    },
 }
 
 export const Characters = [
@@ -871,6 +883,7 @@ export const AUDIO_TRACKS = {
     RACE_COCONUT_MALL: '/soundTracks/COCONUT_MALL_ST.mp3',
     RACE_DELPHINO_SQUARE: '/soundTracks/DELPHINO_SQUARE_ST.mp3',
     RACE_YOSHI_FALLS: '/soundTracks/YOSHI_FALLS_ST.mp3',
+    MOO_MOO_MEADOWS: '/soundTracks/MOO_MOO_MEADOWS_ST.mp3',
 };
 
 export const AUDIO_SFX = {
