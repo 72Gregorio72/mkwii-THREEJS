1a77d25c7224e36124ab750880e88262e9def408
vcastald <valentino.castaldini@gmail.com>
2026-02-16 15:18:09 +0100

    data

--- mario-kart-react/src/components/Data.jsx
+++ mario-kart-react/src/components/Data.jsx
@@ -122,7 +122,7 @@ export const Tracks = {
         file: './Tracks/SNESMarioCircuit/SNESMarioCircuit.glb',
 		preview: '/Previews/SNES Mario Circuit 3.png',
         startPos: [0, 0, 50],
-        checkpoints: './Tracks/DaisyCircuit/DaisyCircuit_checkpoints.glb',
+        checkpoints: './Tracks/DaisyCircuit/SNESMarioCircuit_checkpoint.glb',
         maxCheckpoints: 3,
         soundtrack: 'RACE_MOO_MOO_MEADOWS',
         itemBoxes: './Tracks/DaisyCircuit/DaisyCircuit_itemBox.glb',
