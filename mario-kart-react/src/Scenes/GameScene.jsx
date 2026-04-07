d7a891c33cf68ea82b1d881cf5870a006804e94f
Francescotersi <francescotersillo@gmail.com>
2026-04-07 15:28:32 +0200

    wayponts for DelfinoSquare Fixed

--- mario-kart-react/src/Scenes/GameScene.jsx
+++ mario-kart-react/src/Scenes/GameScene.jsx
@@ -1274,14 +1274,14 @@ export function GameScene({
                     gl.outputColorSpace = THREE.SRGBColorSpace;
                 }}
             >
-                
 
-                {/* <WaypointRecorder
+
+                <WaypointRecorder
                     kartRef={playerRef}
                     isRecording={true}
                 >
 
-                </WaypointRecorder> */}
+                </WaypointRecorder>
 
 
                 <AudioListenerComponent />
